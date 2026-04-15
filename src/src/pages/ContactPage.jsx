86f990b7bd81e89323904e823e5b72e382e067da
moises-design <moises@vistatechsolutions.io>
2026-04-14 23:17:41 -0500

    Cleanup: remove duplicate files accidentally nested inside src/

--- src/src/pages/ContactPage.jsx
+++
@@ -1,2 +0,0 @@
-import Contact from '../components/Contact'
-export default function ContactPage() { return <div style={{paddingTop:'68px'}}><Contact /></div> }
--- src/src/pages/CoveragePage.jsx
+++
@@ -1,2 +0,0 @@
-import Coverage from '../components/Coverage'
-export default function CoveragePage() { return <div style={{paddingTop:'68px'}}><Coverage /></div> }
